908087283dc71f50857ed61928b677b303c35810
LuchoLatella <lucho_latella@hotmail.com>
2024-06-30 14:41:13 -0300

    agregado transacciones.py

--- transacciones.py
+++ transacciones.py
@@ -1,43 +1,26 @@
 
 import json
-import os
 
 def cargar_transacciones():
-    if os.path.exists('data/transacciones.json'):
-        with open('data/transacciones.json', 'r') as archivo:
-            try:
-                return json.load(archivo)
-            except json.JSONDecodeError:
-                return []
-    else:
+    try:
+        with open('transacciones.json', 'r') as file:
+            return json.load(file)
+    except FileNotFoundError:
         return []
 
 def guardar_transacciones(transacciones):
-    with open('data/transacciones.json', 'w') as archivo:
-        json.dump(transacciones, archivo, indent=4)
+    with open('transacciones.json', 'w') as file:
+        json.dump(transacciones, file)
 
 def agregar_transaccion(transacciones, id_vehiculo, id_cliente, tipo, fecha, monto, observaciones):
-    id_transaccion = len(transacciones) + 1
-    transaccion = {
-        "id": id_transaccion,
-        "id_vehiculo": id_vehiculo,
-        "id_cliente": id_cliente,
-        "tipo": tipo,
-        "fecha": fecha,
-        "monto": monto,
-        "observaciones": observaciones
+    nueva_transaccion = {
+        'id': len(transacciones) + 1,
+        'id_vehiculo': id_vehiculo,
+        'id_cliente': id_cliente,
+        'tipo': tipo,
+        'fecha': fecha,
+        'monto': monto,
+        'observaciones': observaciones
     }
-    transacciones.append(transaccion)
+    transacciones.append(nueva_transaccion)
     guardar_transacciones(transacciones)
-
-def editar_transaccion(transacciones, id_transaccion, **kwargs):
-    for transaccion in transacciones:
-        if transaccion['id'] == id_transaccion:
-            transaccion.update(kwargs)
-            guardar_transacciones(transacciones)
-            return
-
-def eliminar_transaccion(transacciones, id_transaccion):
-   transacciones[:] = [transaccion for transaccion in transacciones if transaccion['id'] != id_transaccion]
-   guardar_transacciones(transacciones)
-   return transacciones
