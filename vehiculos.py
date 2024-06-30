0a1131ff94c87a29b5e941aa16dfd6d342276308
LuchoLatella <lucho_latella@hotmail.com>
2024-06-30 14:39:13 -0300

    agregado vehiculos.py

--- vehiculos.py
+++ vehiculos.py
@@ -1,39 +1,37 @@
 
 import json
-import os
 
 def cargar_vehiculos():
-    if os.path.exists('data/vehiculos.json'):
-        with open('data/vehiculos.json', 'r') as archivo:
-            return json.load(archivo)
-    else:
+    try:
+        with open('vehiculos.json', 'r') as file:
+            return json.load(file)
+    except FileNotFoundError:
         return []
 
 def guardar_vehiculos(vehiculos):
-    with open('data/vehiculos.json', 'w') as archivo:
-        json.dump(vehiculos, archivo, indent=4)
+    with open('vehiculos.json', 'w') as file:
+        json.dump(vehiculos, file)
 
 def agregar_vehiculo(vehiculos, patente, marca, modelo, tipo, año, kilometraje, precio_compra, precio_venta, estado):
-    id_vehiculo = len(vehiculos) + 1
-    vehiculo = {
-        "id": id_vehiculo,
-        "patente": patente,
-        "marca": marca,
-        "modelo": modelo,
-        "tipo": tipo,
-        "año": año,
-        "kilometraje": kilometraje,
-        "precio_compra": precio_compra,
-        "precio_venta": precio_venta,
-        "estado": estado
+    nuevo_vehiculo = {
+        'id': len(vehiculos) + 1,
+        'patente': patente,
+        'marca': marca,
+        'modelo': modelo,
+        'tipo': tipo,
+        'año': año,
+        'kilometraje': kilometraje,
+        'precio_compra': precio_compra,
+        'precio_venta': precio_venta,
+        'estado': estado
     }
-    vehiculos.append(vehiculo)
+    vehiculos.append(nuevo_vehiculo)
     guardar_vehiculos(vehiculos)
 
-def editar_vehiculo(vehiculos, id_vehiculo, **kwargs):
+def editar_vehiculo(vehiculos, id_vehiculo, **datos_actualizados):
     for vehiculo in vehiculos:
         if vehiculo['id'] == id_vehiculo:
-            vehiculo.update(kwargs)
+            vehiculo.update(datos_actualizados)
             guardar_vehiculos(vehiculos)
             return
 
